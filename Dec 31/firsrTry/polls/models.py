ad5afc4b574d1ca97ade5e1525962524065609e2
lvnc9 <sam.mohammadi2154@gmail.com>
2023-01-02 02:31:38 +0330

    question & choice models

--- Dec 31/firsrTry/polls/models.py
+++ Dec 31/firsrTry/polls/models.py
@@ -1,3 +1,20 @@
+# Start
+# Modules
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    """ question and publish date """
+    question_text = models.CharField(max_length=200)
+    publish_date = models.DateTimeField("date published")
+
+class Choices(models.Model):
+    """ text of choice and voice tally """
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
+    
+
+
+# End
