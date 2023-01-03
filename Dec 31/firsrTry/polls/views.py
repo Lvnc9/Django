45cc255b3d41bd62adce55e42867e0a1208602a8
lvnc9 <sam.mohammadi2154@gmail.com>
2023-01-03 18:18:05 +0330

    new paths

--- Dec 31/firsrTry/polls/views.py
+++ Dec 31/firsrTry/polls/views.py
@@ -6,3 +6,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hallo this is a test")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at the question {question_id}")
+
+def result(request, question_id):
+    reponse = f"You're looking at the result of question {question_id}"
+    return HttpResponse(reponse)
+
+def vote(request, quesiton_id):
+    return HttpResponse(f"You're voting at the question {quesiton_id}")
