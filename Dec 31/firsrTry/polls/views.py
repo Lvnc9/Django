a6b1ffadafe3d0898a982bef5484e6f44655dbb0
lvnc9 <sam.mohammadi2154@gmail.com>
2023-01-05 03:01:04 +0330

    render and get_object_or_404

--- Dec 31/firsrTry/polls/views.py
+++ Dec 31/firsrTry/polls/views.py
@@ -1,15 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from . models import Question
 
 
+
+# return questions date from Question date saved
 def index(request):
-    return HttpResponse("Hallo this is a test")
+    latest_question_list = Question.objects.order_by("-pub-date")[:5]
+    template = loader.get_template('index.html')
+    context = {
+        "latest_question_list" : latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    
 
+# first style
+#def detail(request, question_id):
+#    return HttpResponse(f"You're looking at the question {question_id}")
 
+# Second style
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at the question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {'quesiton': question})
 
 def result(request, question_id):
     reponse = f"You're looking at the result of question {question_id}"
